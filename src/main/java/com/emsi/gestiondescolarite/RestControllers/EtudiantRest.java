2f2ea3eb22414b980bf7c0b474c3f4a6bd94f1eb
HamzaAitahmed <hamzaaitahmed03@gmail.com>
2024-06-01 20:39:06 +0100

    Rest Added

--- src/main/java/com/emsi/gestiondescolarite/RestControllers/EtudiantRest.java
+++ src/main/java/com/emsi/gestiondescolarite/RestControllers/EtudiantRest.java
@@ -7,14 +7,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("Rest")
 public class EtudiantRest {
 
     @Autowired UserRepository userRepository;
--- src/main/java/com/emsi/gestiondescolarite/RestControllers/GroupsRest.java
+++ src/main/java/com/emsi/gestiondescolarite/RestControllers/GroupsRest.java
@@ -11,14 +11,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("Rest")
 public class GroupsRest {
     @Autowired UserRepository userRepository;
     @Autowired private UserService userService;
